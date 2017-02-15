9b8201b29ebb7421e72596dadd9c8217625c3c2b
Gururaj Rao <gururaja@multicorewareinc.com>
2017-02-15 12:46:15 +0000

    Use the vector conversion (float4 & __m128) operation only when hipcc is used to compile

--- Eigen/src/Core/CoreEvaluators.h
+++ Eigen/src/Core/CoreEvaluators.h
@@ -13,7 +13,9 @@
 #ifndef EIGEN_COREEVALUATORS_H
 #define EIGEN_COREEVALUATORS_H
 
+#if defined(__HIP_DEVICE_COMPILE__)
 #include <hip/hip_vector_types.h>
+#endif
 
 namespace Eigen {
   
@@ -397,6 +399,7 @@ struct nullary_wrapper<Scalar,NullaryOp,false,true,false>
 
 template<typename Scalar,typename NullaryOp>
 struct nullary_wrapper<Scalar,NullaryOp,false,false,false> {
+#if defined(__HIP_DEVICE_COMPILE__)
   template <typename IndexType>
   EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE Scalar operator()(const NullaryOp& op, IndexType i, IndexType j) const { return op(); }
   template <typename IndexType>
@@ -411,6 +414,7 @@ struct nullary_wrapper<Scalar,NullaryOp,false,false,false> {
   return _mm_setr_ps((float)f.x, (float)f.y, (float)f.z, (float)f.w);
   }
   template <typename T, typename IndexType> EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE T packetOp(const NullaryOp& op) const { return op.template packetOp<T>(); }
+#endif
 };
 
 #if 0 && EIGEN_COMP_MSVC>0
