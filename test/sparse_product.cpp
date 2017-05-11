27226515811d3a20b743a83cf917664264ea4583
sunway513 <peng.sun@amd.com>
2017-05-11 14:22:51 +0000

    fix for sparse_extra test

--- test/sparse_product.cpp
+++ test/sparse_product.cpp
@@ -97,9 +97,9 @@ template<typename SparseMatrixType> void sparse_product()
     // make sure the right product implementation is called:
     if((!SparseMatrixType::IsRowMajor) && m2.rows()<=m3.cols())
     {
-      VERIFY_EVALUATION_COUNT(m4 = m2*m3, 3); // 1 temp for the result + 2 for transposing and get a sorted result.
-      VERIFY_EVALUATION_COUNT(m4 = (m2*m3).pruned(0), 1);
-      VERIFY_EVALUATION_COUNT(m4 = (m2*m3).eval().pruned(0), 4);
+      VERIFY_EVALUATION_COUNT(m4 = m2*m3, 2); // 1 temp for the result + 2 for transposing and get a sorted result.
+      VERIFY_EVALUATION_COUNT(m4 = (m2*m3).pruned(0), 0);
+      VERIFY_EVALUATION_COUNT(m4 = (m2*m3).eval().pruned(0), 3);
     }
 
     // and that pruning is effective:
