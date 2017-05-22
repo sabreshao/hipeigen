23b444e5eadb681ccec727fd09f33975f1df6821
sunway513 <peng.sun@amd.com>
2017-05-22 18:02:08 +0000

    Add a missing #endif for EIGEN_HAS_HIP_FP16 guard

--- unsupported/Eigen/CXX11/src/Tensor/TensorReduction.h
+++ unsupported/Eigen/CXX11/src/Tensor/TensorReduction.h
@@ -704,6 +704,7 @@ struct TensorEvaluator<const TensorReductionOp<Op, Dims, ArgType, MakePointer_>,
   template <int NPT, typename S, typename R, typename I> friend void internal::InnerReductionKernel(hipLaunchParm, R, const S, I, I, typename S::CoeffReturnType*);
 
   template <int NPT, typename S, typename R, typename I> friend void internal::OuterReductionKernel(hipLaunchParm, R, const S, I, I, typename S::CoeffReturnType*);
+#endif
 
 #if defined(EIGEN_USE_SYCL)
  template < typename HostExpr_, typename FunctorExpr_, typename Tuple_of_Acc_, typename Dims_, typename Op_, typename Index_> friend class TensorSycl::internal::ReductionFunctor;
